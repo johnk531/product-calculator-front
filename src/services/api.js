c03952627ea4e5a3c974f91953784893381ced6f
John Kennedy <johnkssilva@gmail.com>
2023-09-05 21:48:53 -0300

    Fix: add bearer token in header requests

--- src/services/api.js
+++ src/services/api.js
@@ -4,14 +4,16 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_HOST,
 });
 
-api.interceptors.request.use(async config => {
+api.interceptors.request.use(
+  async config => {
     const token = process.env.REACT_APP_TOKEN;
 
-    if (token) {
-        api.defaults.headers.authorization = `Bearer ${token}`;
-    }
-
-    return config;
-});
+    config.headers['Authorization'] = `Bearer ${token}`;
+        return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
