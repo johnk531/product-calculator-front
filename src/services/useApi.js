3f246531d744ed797117e2c6cf3fb4240251bdb4
John Kennedy <johnkssilva@gmail.com>
2023-09-14 20:21:18 -0300

    Feat: add all user routes

--- src/services/useApi.js
+++ src/services/useApi.js
@@ -18,8 +18,28 @@ api.interceptors.request.use(
 
 const useApi = () => ({
   signin: async (email, password) => {
-      const response = await api.post('/user/signin', { email, password });
+      const response = await api.post(`/user/signin`, { email, password });
       return response.data;
+  },
+  createUser: async (username, email, password) => {
+    const response = await api.post(`/user/create`, { username, email, password });
+    return response.data;
+  },
+  readUser: async (email) => {
+    const response = await api.get(`/user/read/${email}`);
+    return response.data;
+  },
+  updateUser: async (username, email, password) => {
+    const response = await api.put(`/user/update`, { username, email, password });
+    return response.data;
+  },
+  deleteUser: async (email) => {
+    const response = await api.delete(`/user/delete`, { email });
+    return response.data;
+  },
+  listUser: async () => {
+    const response = await api.get(`/user/list`);
+    return response.data;
   }
 })
 
