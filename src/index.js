ce6cbf1356bfce1d6f7b15c690c5c3c2d5ed5c14
John <38889738+johnk531@users.noreply.github.com>
2024-01-03 13:45:28 -0300

    Feature/login -> HML (#29) (#30)
    * Fix: add new logo
    
    * Fix: add new favicon
    
    * Feat: add component logo to login
    
    * Fix: correct logo
    
    * Style: visual improvement on the button
    
    * Feat: add Container component
    
    * Feat: add Form component
    
    * Fix: structure and import components to login
    
    * Feat: add Global component for global styles
    
    * Fix: use the Global component and visual improvement to Button and Form components

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global } from './components/Global.style';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
+  <>    
+    <Global />
     <App />
+  </>
   // </React.StrictMode>
 );
 
--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -1,61 +1,58 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 
 //services
-import useApi from "../../services/useApi";
+import useApi from '../../services/useApi';
 
 //components
+import { Logo } from '../../components/Logo.style';
 import { StyledButton } from '../../components/Button.style';
+import { Container } from '../../components/Container.style';
+import { Form } from '../../components/Form.style';
+
+//assets
+import logo from '../../assets/img/logo-pc.png';
 
 const Login = () =>{
   const api = useApi();
 
-  const [email, setEmail] = useState(""),
-        [password, setPassword] = useState("");
+  const [email, setEmail] = useState(''),
+        [password, setPassword] = useState('');
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
+  const handleLogin = async (e) => {
+    e.preventDefault();
     if (email && password) {
       try {
         const res = await api.signin(email,password);
         console.log(res);
       } catch (error) {
-        console.log("Ocorreu um erro no servidor!", error);
+        console.log('Ocorreu um erro no servidor!', error);
       }
     } else {
-        console.log("email ou senha não preenchidos!");
+        console.log('email ou senha não preenchidos!');
     }
   }
 
   return (
-    <div>
-      <h1>Página de Login</h1>
-      <nav>
-        <ul>
-          <li>
-            teste de login
-          </li>
-          <li>
-            <Link to="/">Voltar a home</Link>
-          </li>
-        </ul>
-      </nav>
-      <div>
-      <form onSubmit={handleLogin}>
-        <label>
-          E-mail:
-          <input type="text" name="email" value={email} onChange={e => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Senha:
-          <input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <StyledButton>Entrar</StyledButton>
-      </form>
-      </div>
-    </div>
+    <Container>
+      <Logo><img src={logo} alt='Logo Product Calculator'/></Logo>
+      <Form>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              Email:     
+              <input type='text' name='email' id='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Set a email' />
+            </label>
+          </div>
+          <div>
+            <label>
+              Password:    
+              <input type='password' name='password' id='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Set a password' />
+            </label>
+          </div>
+          <StyledButton>Entrar</StyledButton>
+        </form>
+      </Form>
+    </Container>
   );
 }
 
